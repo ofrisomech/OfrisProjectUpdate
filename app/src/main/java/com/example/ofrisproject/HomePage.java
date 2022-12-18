216a217e6b2b0a9d110a16ab818267a35cc2bcca
Owner <ofrisomech1@gmail.com>
2022-12-18 11:36:20 +0200

    try again

--- app/src/main/java/com/example/ofrisproject/HomePage.java
+++ app/src/main/java/com/example/ofrisproject/HomePage.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 
 public class HomePage extends AppCompatActivity {
 
-    private int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
