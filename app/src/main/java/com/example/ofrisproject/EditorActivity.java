8f30525ddf82b1cbaee86e4513e89f2be666bfb2
Owner <ofrisomech1@gmail.com>
2023-03-13 15:49:02 +0200

    try again

--- app/src/main/java/com/example/ofrisproject/EditorActivity.java
+++ app/src/main/java/com/example/ofrisproject/EditorActivity.java
@@ -4,26 +4,46 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class EditorActivity extends AppCompatActivity {
-
+    YouTubePlayerView youTubePlayerView;
+    YouTubePlayer youTubePlayer;
+    boolean playerReady = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
-        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        youTubePlayerView = findViewById(R.id.youtube_player_view);
         getLifecycle().addObserver(youTubePlayerView);
-
         youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
             @Override
             public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                String videoId = "XwPEtD0_mx4";
+               String videoId = "XwPEtD0_mx4";
                 youTubePlayer.loadVideo(videoId, 0);
+                playerReady = true;
             }
+
         });
+    }
+
+    public void PlayVideo(View view){
+        if(!playerReady)
+            return;
+        ImageView imageView=(ImageView)view;
+        if(view.getId()==R.id.startVideo) {
+            String videoId = "XwPEtD0_mx4";
+            youTubePlayer.loadVideo(videoId, 0);
+        }
+
+    }
+
+    public void StopVideo(){
+
     }
 }
