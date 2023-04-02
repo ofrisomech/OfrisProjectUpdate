b476bbafa4955e17818da57cefbbf0dd5090c663
ofris <ofrisomech1@gmail.com>
2023-04-02 15:18:33 +0300

    kk

--- app/src/main/java/com/example/ofrisproject/CreateFragment.java
+++ app/src/main/java/com/example/ofrisproject/CreateFragment.java
@@ -105,7 +105,6 @@ public class CreateFragment extends Fragment {
 
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private Song song;
     private DocumentReference songReference;
 
     public void getSongsByGenre() {
