430007ffa317bc825a75754610f2adc9978e9786
ofris <ofrisomech1@gmail.com>
2023-05-23 17:00:41 +0300

    kk

--- app/src/main/java/com/example/ofrisproject/RecordingAdapter.java
+++ app/src/main/java/com/example/ofrisproject/RecordingAdapter.java
@@ -1,6 +1,7 @@
 package com.example.ofrisproject;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,15 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.DatabaseMetaData;
 import java.util.ArrayList;
 
 public class RecordingAdapter extends RecyclerView.Adapter<RecordingAdapter.ViewHolder>  {
@@ -75,6 +85,76 @@ public RecordingAdapter(ArrayList<Recording> list, AdapterCallback activity) {
     public int getItemCount() {
         return recordings.size();
     }
+
+    /*private void IsLikes(String RecId, ImageView imageView){
+        FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference= FirebaseDatabase.getInstance().getRefernce().child("likes").child(RecId);
+        reference.addValueEventListener(new ValueEventListener()){
+            public void onDataChange(DataSnapshot dataSnapshot){
+                if(dataSnapshot.child(firebaseUser.getUid()).exists()) {
+                imageView.setColorFilter(0xffff0000);
+                imageView.setTag("liked");
+
+                }
+                else {
+                    imageView.setColorFilter(0xffff0000);
+                    imageView.setTag("like");
+                }
+            }
+            }
+        });
+    }*/
+
+
+    /* public void displayNumberOfLikes(String postId, String currentUserId){
+        DatabaseReference likesRef = FirebaseDatabase.getInstance().getReference().child('recording').child(postId);
+        likesRef.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    long numOfLikes = 0;
+                    if(dataSnapshot.hasChild("likes")){
+                        numOfLikes = dataSnapshot.child("likes").getValue(Long.class);
+                    }
+
+                    //Populate numOfLikes on post i.e. textView.setText(""+numOfLikes)
+                    //This is to check if the user has liked the post or not
+                    btnLike.setSelected(dataSnapshot.hasChild(userId));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void onLikeClicked(View v, String postId, String userId){
+        DatabaseReference likesRef = FirebaseDatabase.getInstance().getReference().child('Post').child(postId).child("likes");
+        likesRef.addListenerForSingleValueEvent(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long numLikes = 0;
+                if(dataSnapshot.exists()){
+                    numLikes = dataSnapshot.getValue(Long.class);
+                }
+                boolean isLiked = btnLike.isSelected();
+                if(isLiked){
+                    //If already liked then user wants to unlike the post
+                    likesRef.set(numLikes-1);
+                }else {
+                    //If not liked already then user wants to like the post
+                    likesRef.set(numLikes+1);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }*/
 
     public interface AdapterCallback
     {
